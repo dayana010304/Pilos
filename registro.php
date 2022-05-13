d34e61ce04b7bdc1c83472d748117775417b5115
dayanacossio <71185178+dayana010304@users.noreply.github.com>
2022-05-13 15:05:35 -0500

    Actualizacion

--- registro.php
+++ registro.php
@@ -31,7 +31,7 @@ if (isset($_POST["botonEnvio"]))
 
     $consultaSQL="INSERT INTO Estudiantes (identificacion, fechaVencimiento) VALUES ('$identificacion', '$fechaVencimiento')";
 
-    $transaccion->agregarDatos($consultaSQL);    
+    $transaccion->agregarDatos($consultaSQL);        
 }
 
 ?>
